b912392d01eef0372ec068c99d54d36b6bf92bef
David Wang <dwang@viewtrak.com>
2018-03-26 15:51:24 -0600

    first commit whole project frame

---
+++ GitTraining/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitTraining.Startup))]
+namespace GitTraining
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
